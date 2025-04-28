e237a8319dbccb00fb9a7ae290622a75796b641e
Raas-Farooq <raasfarooq@gmail.com>
2025-04-28 22:28:42 +0500

    add reactquill

--- frontend/magicalPosts/src/components/editPost.jsx
+++ frontend/magicalPosts/src/components/editPost.jsx
@@ -1,13 +1,15 @@
 import { useEffect, useState } from "react";
 import { useLocation, useParams } from "react-router-dom";
 import VITE_API_URL from '../config';
+import ReactQuill from "react-quill";
+import 'react-quill/dist/quill.snow.css';
 
 const EditPost = () => {
     const location = useLocation();
     const {id} = useParams();
     const [post, setPost]= useState(location.state?.post || null);
     const [loadingPost, setLoadingPost] = useState(null);
-    const [postContent, setPostContent] = useState(null);
+    const [quillContent, setQuillContent] = useState([]);
     const [postData, setPostData] = useState({
         title:'',
         titleImage:'',
@@ -25,6 +27,7 @@ const EditPost = () => {
                 const response = axios.get(`${VITE_API_URL}/blog/getPost/${id}`);
 
                 if(response.data.success){
+                    
                     setPost(response.data.blog);
                 }
             }catch(err){
@@ -36,7 +39,23 @@ const EditPost = () => {
         }
         fetchPost();
        }
+       const escapeRegExp = (string) => {
+        return string.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
+    }
        if(post){
+        let bodyText = post.content[0].textContent;
+        // console.log("initial ", bodyText)
+        Object.entries(post.contentImagesUrls).forEach(([placeholder,url]) => {
+            console.log("placeholder: inside entry ", placeholder);
+            const data_id = placeholder.match(/!\[image\]\((\d+)\)/)[1];
+            const replacement=`<img src="${url}" alt="${placeholder}" className="h-44 w-44" data-id="${data_id}" />`;
+            
+            bodyText = bodyText.replace(RegExp(escapeRegExp(placeholder), 'g'), replacement);
+            console.log("bodyText inside entries; ", bodyText);
+        })
+        console.log("bodyText after Object.enties: ", bodyText);
+        // console.log("bodyText: after replace", bodyText);
+        setQuillContent(bodyText)
         setPostData(prev => ({
             ...prev,
             title:post?.title || '',
@@ -49,11 +68,10 @@ const EditPost = () => {
     },[post])
 
     // useEffect(() => {
-    //     if(post.content){
-    //         const content = post.content[0].textContent;
-    //         setPostContent(content);
+    //     if(quillContent){
+    //         console.log("quill content: ", quillContent);
     //     }
-    // },[])
+    // },[quillContent])
 
 
     function handleTitleChange(e){
@@ -65,12 +83,43 @@ const EditPost = () => {
         }))
         localStorage.setItem('title', JSON.stringify(newTitle));
     }
+
+    const handleContentChange = function(newContent){
+        setQuillContent(newContent)
+        setPostData(prev => ({
+            ...prev,
+            content:[{textContent:newContent}]
+        }))
+    }
     return (
-        <div>
+        <div className="prose prose-lg max-w-full">
+            {console.log("quillContent: DOm", quillContent)}
             {loadingPost && <h1> Post is being Loading..</h1>}
-            {console.log("Post.PostDAta: ", postData)}
-            {/* <h1> {post?.title} </h1> */}
-            <form type="Post" className="text-left space-y-4">
+            <ReactQuill
+                theme="snow"
+                value={quillContent}
+                onChange={handleContentChange}
+                className="space-y-2"
+                modules={{
+                    toolbar:[
+                        [{ header: [1, 2, false] }],
+                        ['bold', 'italic', 'underline'],
+                        ['image', 'code-block'],
+                        [{ list: 'ordered' }, { list: 'bullet' }],
+                        ['clean']
+                    ]
+                }}
+
+            />
+            
+        </div>
+    )
+}
+
+export default EditPost
+
+
+{/* <form type="Post" className="text-left space-y-4">
                 <label> Edit Your title  </label>
                 <input type='text' name='title' value={postData.title} className="border border-gray-700 w-1/2" 
                 onChange={handleTitleChange} />
@@ -82,9 +131,4 @@ const EditPost = () => {
                     <textarea cols={55} rows={18} className="border border-gray-700" value={postData.content} />
                 </div>
                 
-            </form>
-        </div>
-    )
-}
-
-export default EditPost
+            </form> */}
